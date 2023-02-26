dca3462d67e292af78dcbc36a6e4cb3cd86ce8b6
NoNameLmao <egor5553049@gmail.com>
2023-02-26 20:54:28 +0300

    another rewrite i guess lmao

--- discord_token_checker.py
+++ discord_token_checker.py
@@ -2,7 +2,14 @@ import base64
 import random
 import requests
 
-def generate_discord_token(user_id):
+# Check if id.txt exists and get the user ID from it if it does
+try:
+    with open("id.txt", "r") as f:
+        user_id = f.read().strip()
+except FileNotFoundError:
+    user_id = input("Enter a Discord user ID: ")
+
+while True:
     # Encode the user ID in base64
     b64_user_id = base64.b64encode(user_id.encode()).decode()
 
@@ -15,33 +22,22 @@ def generate_discord_token(user_id):
 
     # Check the response status code
     if response.status_code == 200:
-        # If the status code is 200, the token is valid, so return it
-        return token
-    elif "rate limited" in response.text:
-        # If the response contains "rate limited", raise an exception
-        raise Exception("API rate limit reached. Please try again later.")
-    else:
-        # If the response does not contain "rate limited", raise an exception with the error message
-        raise Exception(f"Token {token} is invalid. Error message: {response.text}")
-
-# Check if id.txt exists and get the user ID from it if it does
-try:
-    with open("id.txt", "r") as f:
-        user_id = f.read().strip()
-except FileNotFoundError:
-    user_id = input("Enter a Discord user ID: ")
-
-# Generate a token and check if it's valid
-token = generate_discord_token(user_id)
-while True:
-    try:
-        # If the token is valid, write it to valid.txt and exit the loop
+        # If the status code is 200, the token is valid, so write it to valid.txt and exit
         with open("valid.txt", "a") as f:
             f.write(f"{token}\n")
         print(f"Token {token} is valid.")
         break
-    except Exception as e:
-        # If the token is invalid, print the error message and generate a new token
-        print(str(e))
-        print("Generating a new token...")
-        token = generate_discord_token(user_id)
+    else:
+        # If the status code is not 200, the token is invalid
+        if "rate limited" in response.text:
+            # If the response contains "rate limited", exit the program
+            print("API rate limit reached. Please try again later.")
+            break
+        else:
+            # If the response does not contain "rate limited", print the error message and repeat the token generation process
+            print(f"Token {token} is invalid.")
+            try:
+                error_message = response.json()["message"]
+            except:
+                error_message = response.text
+            print(f"Error message: {error_message}")
